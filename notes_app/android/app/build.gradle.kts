0ab43c02910667aa8ea1bb61c00fd7c5aa266185
tonywahome <a.wambugu@alustudent.com>
2025-07-06 23:21:08 +0200

    last update

--- notes_app/android/app/build.gradle.kts
+++ notes_app/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.notes_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // Changed from 21 to 23
+        minSdk = 23 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
